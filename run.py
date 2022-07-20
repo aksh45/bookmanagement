3cfe35d978111fb88c55ad0315d702d46fd5635e
aksh45 <ahujaakshit20@gmail.com>
2022-07-20 15:36:56 +0530

    added env variables

--- run.py
+++ run.py
@@ -10,5 +10,8 @@ config.read(os.path.abspath(os.path.join(".ini")))
 
 if __name__ == "__main__":
     app.config['DEBUG'] = True
-    app.config['MONGO_URI'] = config['PROD']['DB_URI']
+    app.config['MONGO_URI'] = os.environ.get('MONGO_URI','')
+    app.run()
+else:
+    app.config['MONGO_URI'] = os.environ.get('MONGO_URI','')
     app.run()
